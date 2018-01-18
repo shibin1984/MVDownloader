8527c1524caacf6a14bba80efb48bca76489500a
Shibin Moideen <shibin.moideen@cabotsolutions.com>
2018-01-18 14:44:03 +0530

    Initial Commit

---
+++ MVDownloader/MVDownloader.h
@@ -0,0 +1,19 @@
+//
+//  MVDownloader.h
+//  MVDownloader
+//
+//  Created by Shibin Moideen on 1/18/18.
+//  Copyright © 2018 Shibin Moideen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVDownloader.
+FOUNDATION_EXPORT double MVDownloaderVersionNumber;
+
+//! Project version string for MVDownloader.
+FOUNDATION_EXPORT const unsigned char MVDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVDownloader/PublicHeader.h>
+
+
